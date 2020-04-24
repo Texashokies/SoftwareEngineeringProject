44be7ae7ea7783608bfa874c6425081ca777c40e
Texashokies <43481909+Texashokies@users.noreply.github.com>
2020-04-23 21:32:15 -0500

    Created Issues page that is unfinished.
    Created Issues webpage, unifinished, contains template for bug report card that should go into the collapsables.

---
+++ public/scripts/issues.js
@@ -0,0 +1,48 @@
+//Setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+    
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+  
+    var dropdowns = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(dropdowns, {constrainWidth : false});
+
+    var collapsables = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsables);
+
+    var selections = document.querySelectorAll('select');
+    M.FormSelect.init(selections);
+
+    setupUIIndex();
+
+});
+
+const loggedOutComponents = document.querySelectorAll('.logged-out');
+const loggedInComponents = document.querySelectorAll('.logged-in');
+
+const setupUIIndex = (user) => {
+    if(user) {
+        //Toggle UI elements
+        loggedInComponents.forEach(item => item.style.display = "block");
+        loggedOutComponents.forEach(item => item.style.display = 'none');
+    }
+    else{
+        //Toggle UI elements
+        loggedInComponents.forEach(item => item.style.display = "none");
+        loggedOutComponents.forEach(item => item.style.display = 'block');
+    }
+}
+
+var firebaseConfig = {
+    apiKey: "AIzaSyC6QR79tnkpGKfvhc1d_VM2Pdp8lmwVTSw",
+    authDomain: "software-engineering-pro-3ba1c.firebaseapp.com",
+    databaseURL: "https://software-engineering-pro-3ba1c.firebaseio.com",
+    projectId: "software-engineering-pro-3ba1c",
+    storageBucket: "software-engineering-pro-3ba1c.appspot.com",
+    messagingSenderId: "482967068895",
+    appId: "1:482967068895:web:559182a15c3ba3c1e5c96a"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const database = firebase.database();
