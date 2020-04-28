bf6ba17dfd7616e2836d99c7b0c10fa8ff03a66c
Texashokies <43481909+Texashokies@users.noreply.github.com>
2020-04-27 21:44:19 -0500

    Contacts display code wip, waiting for firebase admin package to install for further development.

--- server.js
+++ server.js
@@ -4,9 +4,24 @@ const express = require('express');
 const CloudmersiveVirusApiClient = require('cloudmersive-virus-api-client');
 const formidable = require('formidable');
 const fs = require('fs');
+var admin = require('firebase-admin');
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://software-engineering-pro-3ba1c.firebaseio.com"
+});
+
 var app = express();
 app.use(express.static(__dirname + '/public'));
 var port = 8000;
 app.listen(port);
 console.log("server on " + port);
 app.use(express.json());
+
+app.post('/getuser' , (request,response) => {
+    console.log(request.body);
+    const data = request.body;
+    var id = data.id;
+    console.log(id);
+})
